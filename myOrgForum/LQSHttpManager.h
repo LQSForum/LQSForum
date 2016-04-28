c56ba60e2b8f854c478fea9754e05c8af0ab9a8f
Yirenduishou <zJob_shuang@163.com>
2016-04-28 18:13:28 +0800

    update

---
+++ myOrgForum/LQSHttpManager.h
@@ -0,0 +1,13 @@
+//
+//  LQSHttpManager.h
+//  myOrgForum
+//
+//  Created by SkyAndSea on 16/4/28.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LQSHttpManager : NSObject
+
+@end
---
+++ myOrgForum/LQSHttpManager.m
@@ -0,0 +1,13 @@
+//
+//  LQSHttpManager.m
+//  myOrgForum
+//
+//  Created by SkyAndSea on 16/4/28.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import "LQSHttpManager.h"
+
+@implementation LQSHttpManager
+
+@end
