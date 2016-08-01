f533f800513f06e1c9fd00f05b0d0890a48d5049
948215969@qq.com <yirenduishou>
2016-08-01 22:48:53 +0800

    update

--- myOrgForum/LQSHorizonDataModel.h
+++ myOrgForum/LQSHorizonDataModel.h
@@ -9,5 +9,19 @@
 #import <Foundation/Foundation.h>
 
 @interface LQSHorizonDataModel : NSObject
+@property (nonatomic,strong)NSString * rs;
+@property (nonatomic,strong)NSString * errcode;
+@property (nonatomic,strong)NSArray * head;
+@property (nonatomic,strong)NSArray * body;
+@property (nonatomic,strong)NSArray * piclist;
+@property (nonatomic,strong)NSString * page;
+@property (nonatomic,strong)NSString *   has_next;
+
+
+
+
+
+
+
 
 @end
