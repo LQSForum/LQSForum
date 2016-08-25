868939a107f1fc1409baf2e3132e6b44af7bf3f4
阿凡树 <729397005@qq.com>
2016-08-25 22:33:28 +0800

    新建论坛模块二级页面   ------ commit by afantree

---
+++ myOrgForum/forum/Cell/LQSForumDetailTopCell.h
@@ -0,0 +1,13 @@
+//
+//  LQSForumDetailTopCell.h
+//  myOrgForum
+//
+//  Created by 阿凡树 on 16/8/25.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LQSForumDetailTopCell : UITableViewCell
+
+@end
---
+++ myOrgForum/forum/View/LQSForumDetailHeadView.h
@@ -0,0 +1,13 @@
+//
+//  LQSForumDetailHeadView.h
+//  myOrgForum
+//
+//  Created by 阿凡树 on 16/8/25.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LQSForumDetailHeadView : UIView
+
+@end
