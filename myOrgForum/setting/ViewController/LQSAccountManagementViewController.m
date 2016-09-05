95a290ee1ef727bc46c5860b6efa48cb8711a2d0
948215969@qq.com <yirenduishou>
2016-09-05 23:17:45 +0800

    登出

--- myOrgForum/setting/ViewController/LQSAccountManagementViewController.m
+++ myOrgForum/setting/ViewController/LQSAccountManagementViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "LQSAccountManagementViewController.h"
+#import "LQSUserManager.h"
+@interface LQSAccountManagementViewController ()<UITableViewDelegate,UITableViewDataSource,UIAlertViewDelegate>
+{
+    UITableView *_tableView;
 
-@interface LQSAccountManagementViewController ()
 
+}
 @end
 
 @implementation LQSAccountManagementViewController
@@ -17,22 +21,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"账号管理";
+    [self createTableView];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)createTableView{
+
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight) style:UITableViewStyleGrouped];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+
+    [self.view addSubview:_tableView];
+
+
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - tableViewDelegate&DataSource
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+
 }
-*/
 
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    return 1;
+
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+static NSString *identifier = @"accountIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+    }
+    
+    cell.textLabel.text = @"登出";
+    return cell;
+
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"确定要退出登录吗？" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+    [alert show];
+
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if (buttonIndex == 1) {
+        [LQSUserManager clearUser];
+        [kAppDelegate showHUDMessage:@"登出成功" hideDelay:2];
+    }
+
+
+
+}
 @end
