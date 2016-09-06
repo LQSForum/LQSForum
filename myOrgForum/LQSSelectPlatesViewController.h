5bd65964c5cd507d47fb127ad2b3beb64ea5e18f
948215969@qq.com <yirenduishou>
2016-09-07 00:05:58 +0800

    模块Vc

---
+++ myOrgForum/LQSSelectPlatesViewController.h
@@ -0,0 +1,13 @@
+//
+//  LQSSelectPlatesViewController.h
+//  myOrgForum
+//
+//  Created by 周双 on 16/9/6.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//选择板块控制器
+
+#import "rootViewController.h"
+
+@interface LQSSelectPlatesViewController : rootViewController
+
+@end
