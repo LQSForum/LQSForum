bc6222c54da3bb86dde2aee2a714a175a438e247
xgunix <zhizhangfashi@foxmail.com>
2017-03-24 06:20:21 +0800

    1.在kappdelegate里添加了一个显示网络连接时的indicator 2.在LQSHTMLVC里添加了网络加载时的HUD效果。

--- myOrgForum/LQSAppDelegate+HUD.m
+++ myOrgForum/LQSAppDelegate+HUD.m
@@ -30,18 +30,39 @@
     return hud;
 
 }
+//创建HUD
+- (MBProgressHUD *)makeHUDAtView:(UIView *)view
+                         message:(NSString *)message hudMode:(MBProgressHUDMode )mode{
+    
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+    hud.margin = 10.0;
+    hud.bezelView.layer.cornerRadius = 5.0;
+    hud.opacity = 0.5;
+    hud.yOffset -= 40;
+    if (message && ![message isEqualToString:@""]) {
+        hud.labelText = message;
+        hud.mode = mode;
+    }
+    return hud;
+    
+}
 
 //移除HUD
 - (void)removeHUD:(MBProgressHUD *)hud
             delay:(NSTimeInterval)delay{
 
     if (hud) {
-        [hud hide:YES afterDelay:delay];
+//        [hud hide:YES afterDelay:delay];
+        [hud hideAnimated:YES afterDelay:delay];
         [hud removeFromSuperViewOnHide];
         hud = nil;
     }
 }
-
+-(void) showIndicatorWithMessage:(NSString *)msg hudMode:(MBProgressHUDMode )mode{
+    [self removeHUD:self.viewHUD delay:0];
+    self.viewHUD = [self makeHUDAtView:self.window
+                               message:msg hudMode:mode];
+}
 //显示HUD信息
 - (void)showHUDMessage:(NSString *)message
              hideDelay:(NSTimeInterval)delay{
--- myOrgForum/introduce/introduceVC/LQSHTMLViewController.m
+++ myOrgForum/introduce/introduceVC/LQSHTMLViewController.m
@@ -187,7 +187,22 @@
     [self.navigationController pushViewController:htmlVc animated:YES];
 
 }
-
+#pragma mark - WKNavigationDelegate
+// 页面开始加载时调用
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
+    [kAppDelegate showIndicatorWithMessage:@"loading" hudMode:MBProgressHUDModeIndeterminate];
+}
+// 当内容开始返回时调用
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
+}
+// 页面加载完成之后调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    [kAppDelegate removeHUDDelay:0.0];
+}
+// 页面加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+    [kAppDelegate showHUDMessage:@"网络连接失败，请刷新重试" hideDelay:1.0];
+}
 #pragma mark - getter方法
 - (NSMutableArray *)videoData{
     if (_videoData == nil) {
