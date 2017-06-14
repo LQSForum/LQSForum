02992e4ec22e9fb6b6ec22f5f6a163a7dfd09442
wangbokevin <wangbokevin@didichuxing.com>
2017-06-14 19:31:54 +0800

    [推荐]添加cell文件

---
+++ myOrgForum/HomePage/Recommend/Views/LQSRecommendListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LQSRecommendListTableViewCell.h
+//  myOrgForum
+//
+//  Created by wangbo on 2017/6/14.
+//  Copyright © 2017年 SkyAndSea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LQSRecommendListTableViewCell : UITableViewCell
+
+@end
---
+++ myOrgForum/HomePage/Recommend/Views/LQSRecommendListTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  LQSRecommendListTableViewCell.m
+//  myOrgForum
+//
+//  Created by wangbo on 2017/6/14.
+//  Copyright © 2017年 SkyAndSea. All rights reserved.
+//
+
+#import "LQSRecommendListTableViewCell.h"
+
+@implementation LQSRecommendListTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
